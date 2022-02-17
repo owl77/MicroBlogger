08089c1172ef2c072cc8240a385f6fd18f296539
owl77 <41900928+owl77@users.noreply.github.com>
2022-02-17 16:15:57 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 blogname="MyBlog"
 name="test"
-sed -i 's/NAME/'$name'/' NAMEedit.php
-sed -i 's/NAME/'$name'/' NAMEblog.php
-sed -i 's/NAME/'$name'/' NAMElogin.php
-sed -i 's/NAME/'$name'/' NAMEmessenger.php
-sed -i 's/NAME/'$name'/' NAMEmessview.php
-sed -i 's/NAME/'$name'/' NAMEnew.php
-sed -i 's/BLOGNAME/'$blogname'/' NAMEblog.php
-sed -i 's/BLOGNAME/'$blogname'/' NAMEmessenger.php
+
+
+sed -i 's/NAME/${name}/' NAMEedit.php
+sed -i 's/NAME/${name}/' NAMEblog.php
+sed -i 's/NAME/${name}/' NAMElogin.php
+sed -i 's/NAME/${name}/' NAMEmessenger.php
+sed -i 's/NAME/${name}/' NAMEmessview.php
+sed -i 's/NAME/${name}/' NAMEnew.php
+sed -i 's/BLOGNAME/${blogname}/' NAMEblog.php
+sed -i 's/BLOGNAME/${blogname}/' NAMEmessenger.php
 mv NAMEedit.php $name"edit.php"
 mv NAMEblog.php $name"blog.php"
 mv NAMElogin.php $name"login.php"
